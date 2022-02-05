0514818fb2391331062be2b4b04ef93a3364acdd
Mustafa AKIN <mstf.akin@outlook.com>
2022-02-05 10:11:18 +0300

    search terms router added

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from bidnamic.campaigns.views import CampaignViewSet
+from bidnamic.search_terms.views import SearchTermViewSet
 from bidnamic.users.api.views import UserViewSet
 from bidnamic.ad_groups.views import AdGroupViewSet
 
@@ -13,6 +14,7 @@ else:
 router.register("users", UserViewSet)
 router.register("campaigns", CampaignViewSet)
 router.register("ad_groups", AdGroupViewSet)
+router.register("search_terms", SearchTermViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
